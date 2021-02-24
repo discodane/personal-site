14ad2910fb8469a02649efe4e06cc98141c36e36
Dane Erickson <Dane.Erickson@jbhunt.com>
2021-02-24 10:31:20 -0500

    Added stack overflow link

--- src/assets/aboutList.js
+++ src/assets/aboutList.js
@@ -4,11 +4,12 @@ export default [
   "I've been to China",
   "I used to sing a lot",
   "I used to live in Uruguay. Vamo Arriba no ma",
-  "I'm married with a kid and dog",
+  "I'm married with a dog and a kid with another on the way",
   "My wife is awesome. She teaches economics and researches the economics in education",
   <span className='linkText' onClick={() => window.open('https://github.com/discodane', '_blank')}>Here I have a lot of ambitious/unfinished work</span>,
   "I used to be a full stack developer at Walmart, where we helped people buy stuff",
   "I worked as a full stack developer at iFit, where we helped people move",
   "I currently help develop an app called Drive at J.B. Hunt, which helps people move... stuff",
   <span className='linkText' onClick={() => window.open('https://docs.google.com/document/d/1eOoBIPB7-oQ2ob2iFZtwx2VK9y2p9bL_AtXaP-JE50Y/export?format=pdf', '_blank')}>Extra details about my exciting work life can be found here</span>,
+  <span className='linkText' onClick={() => window.open('https://stackoverflow.com/users/2665518/discodane', '_blank')}>I have almost 2000 reputation points on stackoverflow!</span>
 ]
